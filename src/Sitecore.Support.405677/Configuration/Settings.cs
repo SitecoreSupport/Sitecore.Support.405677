064fd337f85c7708991a742a3a265abb1e07c3f7
valeriyleman <vle@sitecore.net>
2017-05-05 16:25:51 +0300

    Initial fix

---
+++ src/Sitecore.Support.405677/Configuration/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace Sitecore.Support.ContentSearch.SolrProvider.Configuration
+{
+  public class Settings
+  {
+    public static int ConnectionTimeout => Sitecore.Configuration.Settings.GetIntSetting("ContentSearch.Solr.ConnectionTimeout", 60000);
+
+    public static string IndexConstructorParametersQuery => Sitecore.Configuration.Settings.GetSetting(
+        "ContentSearch.Solr.IndexConstractorParameters.Query",
+        "contentSearch/configuration/indexes/index/param[@desc='collection' or @desc='rebuildcollection']");
+  }
+}
