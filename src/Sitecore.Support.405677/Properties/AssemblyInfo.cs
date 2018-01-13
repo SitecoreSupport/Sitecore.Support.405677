b950aace40c1a14d2e282f2c4a71311ad853197b
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-01-13 15:32:22 +0200

    Add project files

---
+++ src/Sitecore.Support.405677/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.405677")]
+[assembly: AssemblyProduct("Sitecore.Support.405677")]
+[assembly: ComVisible(false)]
